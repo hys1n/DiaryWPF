7039adeef8ec596dee874114972a84ee74ac3464
hys1n <nikita.linnyk@nure.ua>
2024-06-05 18:52:37 +0300

    Added calendar view

---
+++ DiaryWPF/Views/CalendarView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DiaryWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for CalendarView.xaml
+    /// </summary>
+    public partial class CalendarView : UserControl
+    {
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
